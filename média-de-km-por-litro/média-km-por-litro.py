99be3c2e7c38ec9d8e68dcbb2af47d698e2dec5d
Felipe Santos <s3t.felipe@gmail.com>
2025-11-22 15:27:18 -0300

    Função média por km completa

---
+++ média-de-km-por-litro/média-km-por-litro.py
@@ -0,0 +1,9 @@
+print("média de KM por litros que seu viculo esta fazendo")
+
+litros= int (input("Digite a quantidade de litros que foram ultilizados: "))
+KM= int (input("Digite quantos km foram percorridos: "))
+
+resultado= (litros / KM)
+
+print("O seu veiculo fez:",resultado,"por KM.")
+print("Precione ENTER para finalizar o programa ")
